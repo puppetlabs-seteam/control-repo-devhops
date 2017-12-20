7ed5b32ee3398389ac5dc0b734fd446a536534b7
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2017-12-20 17:09:53 +0100

    added notify

--- site/profile/manifests/redact.pp
+++ site/profile/manifests/redact.pp
@@ -5,7 +5,8 @@ class profile::redact (
 )
 {
   # this call will display the proper output, but because it's not a resource the string won't exist in the catalog.
-  notice("Class['redact_test'] was called with the following parameters ${a}, ${b}, ${c}")
+  notice("Class['redact'] was called with the following parameters ${a}, ${b}, ${c}")
+  notify { "Class['redact'] was called with the following parameters ${a}, ${b}, ${c}": }
 
   # but the catalog won't record what the passed in param was.
   redact('a')
