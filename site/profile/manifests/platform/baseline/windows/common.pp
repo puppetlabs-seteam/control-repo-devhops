e78e1ef4cd2795c30f3f6a2d1e104d9829033835
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-22 16:35:21 +0200

    Update common.pp

--- site/profile/manifests/platform/baseline/windows/common.pp
+++ site/profile/manifests/platform/baseline/windows/common.pp
@@ -5,15 +5,25 @@ class profile::platform::baseline::windows::common {
     timeout => 15,
   }
 
-registry_key { 'HKLM\SOFTWARE\Policies\Microsoft\Windows Defender':
+  registry_key { 'HKLM\SOFTWARE\Policies\Microsoft\Windows Defender':
     ensure => present,
     before => Registry_value['DisableAntiSpyware'],
   }
 
-registry_value { 'DisableAntiSpyware':
+  registry_value { 'DisableAntiSpyware':
     path   => 'HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\DisableAntiSpyware',
     ensure => present,
     type   => dword,
     data   => 1,
+  }
+
+  $query    = "inventory[facts] { facts.hostname ~ 'windchops' }"
+  $response = puppetdb_query($query)
+  $dcip   = $response[0]['facts']['ipaddress']
+
+  dsc_xdnsserveraddress { 'dnsserveraddress':
+    dsc_address        => $dcip,
+    dsc_interfacealias => 'ethernet',
+    dsc_addressfamily  => 'ipv4',
   }
 }
