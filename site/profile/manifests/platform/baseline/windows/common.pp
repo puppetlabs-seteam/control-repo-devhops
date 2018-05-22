82af12c427393620538ffc863a4b4827b3e5a4c0
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-22 15:50:11 +0200

    Update common.pp

--- site/profile/manifests/platform/baseline/windows/common.pp
+++ site/profile/manifests/platform/baseline/windows/common.pp
@@ -5,4 +5,15 @@ class profile::platform::baseline::windows::common {
     timeout => 15,
   }
 
+registry_key { 'HKLM\SOFTWARE\Policies\Microsoft\Windows Defender':
+    ensure => present,
+    before => Registry_value['DisableAntiSpyware'],
+  }
+
+registry_value { 'DisableAntiSpyware':
+    path   => 'HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\DisableAntiSpyware',
+    ensure => present,
+    type   => dword,
+    data   => 1,
+  }
 }
