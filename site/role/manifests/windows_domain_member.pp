24d563c4ab2edf5f5b16c4333c4c5357d35e33d2
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-22 17:47:06 +0200

    Update and rename devhops_domain_member.pp to windows_domain_member.pp

--- site/role/manifests/windows_domain_member.pp
+++ site/role/manifests/windows_domain_member.pp
@@ -1,7 +1,7 @@
 # DEVHops Demo
 # Baseline Configuration plus Windows Domain Member
 #
-class role::devhops_domain_member {
+class role::windows_domain_member {
   include profile::platform::baseline
   include profile::platform::domain_member
 }
